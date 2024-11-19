b878ddee6fb1de40d6a1fbf5fa9c283800756bf7
Chandru AIML-A <231501031@rajalakshmi.edu.in>
2024-11-20 00:02:00 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+
+package pkgfinal;
+
+public class Final {
+
+    public static void main(String[] args) {
+        login obj1=new login();
+        obj1.setVisible(true);
+        obj1.setLocationRelativeTo(null);
+    }
+    
+}
